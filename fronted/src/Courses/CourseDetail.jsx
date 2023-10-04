f62867d969d9a87cfa9afe1c449a5dc66d266c4d
dreamAI0129 <robertwang3129@gmail.com>
2023-10-04 22:49:27 +0530

    new box

--- fronted/src/Courses/CourseDetail.jsx
+++ fronted/src/Courses/CourseDetail.jsx
@@ -8,15 +8,20 @@ const CourseDetail = () => {
     const course = courses.filter((c) => c.title === title)[0]
     return (
         <>
-            <Box bg='#6E71CC' className='relative overflow-hidden text-center p-2' >
-                <Heading color='white'>Become a {title.split(" ").map(t => t[0].toUpperCase() + t.substring(1)).join(" ")}</Heading>
-                <Heading color='yellow.400'>(Extensive Learning Course)<Badge colorScheme='yellow' fontSize={'xl'} borderRadius={'10em'} color='yellow.600' p={'5px 15px'} className='text-lg rounded-lg text-center flex justify-center items-center ml-2'>Full Time</Badge></Heading>
-                <Text className='text-white text-xl max-h-10'>
-                    Become a job-ready {title} in {course.duration} weeks.<br/> Learn at ₹0 upfront fee; pay after placement.
-                </Text>
+            <Box bg='#6E71CC' className='relative overflow-hidden text-center p-2' h='300px' >
                 <Box bg='#B7B8E5' className='hidden md:flex items-center justify-center w-[248px] h-[248px] pointer-events-none bg-white absolute top-[35%] rounded-[100%] right-[-2%] z-[0]'>
                     <Image className="max-h-[94px] max-w-[104px] w-full object-cover" src='https://masai-website-images.s3.ap-south-1.amazonaws.com/full_stack1_329d6b5595.png' />
                 </Box>
+                <Heading mt='60px' color='white'>Become a {title.split(" ").map(t => t[0].toUpperCase() + t.substring(1)).join(" ")}</Heading>
+                <Heading color='yellow.400' mt='2'>(Extensive Learning Course)<Badge colorScheme='yellow' fontSize={'xl'} borderRadius={'10em'} color='yellow.600' p={'5px 15px'} className='text-lg rounded-lg text-center flex justify-center items-center ml-2'>Full Time</Badge></Heading>
+                <Text className='text-white text-2xl mt-5 max-h-10'>
+                    Become a job-ready {title} in {course.duration} weeks.<br/> Learn at ₹0 upfront fee; pay after placement.
+                </Text>
+            </Box>
+            <Box className='pt-8 md:pt-6 pb-8 md:pb-0 bg-[#F7F7FF]'>
+                <div className='max-w-[1440px] mx-auto flex flex-col md:flex-row md:justify-center'>
+                    
+                </div>
             </Box>
         </>
     )
