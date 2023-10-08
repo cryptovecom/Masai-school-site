4a9ae31d5951331563c52bdb72c886bd2ac54f00
dreamAI0129 <robertwang3129@gmail.com>
2023-10-08 19:43:44 +0530

    file

--- fronted/src/index.js
+++ fronted/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react'
