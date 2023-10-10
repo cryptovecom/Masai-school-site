899b928d84ac74064129aafca77bbcef6ac38c4b
dreamAI0129 <robertwang3129@gmail.com>
2023-10-10 15:38:37 +0530

    F

--- fronted/src/Events/Events.jsx
+++ fronted/src/Events/Events.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../Style/Events.css";
+import "../Style/Events.css"
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 const Events = () => {
