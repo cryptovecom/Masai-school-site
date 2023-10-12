ddcf0c5a9dacc7fad02634f11fb7a3ee8714371f
dreamAI0129 <robertwang3129@gmail.com>
2023-10-12 16:04:59 +0530

    submit

--- fronted/src/allRoutes.jsx
+++ fronted/src/allRoutes.jsx
@@ -4,11 +4,8 @@ import CoursePage from './Courses/CoursePage'
 import CourseDetail from './Courses/CourseDetail'
 import Events from './Events/Events'
 import Homes from './components/Homes'
-<<<<<<< HEAD
 import LoginAdmin from './AdminPanel/LoginAdmin'
-=======
 // import Referal from './ReferalComp/Referal'
->>>>>>> 51f3b14659ab4661912f259e12fdcb3f4c670451
 import EventDetails from './Events/EventDetails'
 import Referal from './ReferalComp/referal'
 
@@ -21,11 +18,8 @@ const AllRoutes = () => {
         <Route path='/event/getevents/:id' element={<EventDetails/>} />
         <Route path='/'element={<Homes/>}/>
         <Route path='/courses/:idx' element={<CourseDetail />} />
-<<<<<<< HEAD
         <Route path='/admin' element={<LoginAdmin/>}/>
-=======
         <Route path='/fees' element={<Referal/>} />
->>>>>>> 51f3b14659ab4661912f259e12fdcb3f4c670451
     </Routes>
   )
 }
