604c26b9596fa386691dfbcf8fc272bc98681aa4
dreamAI0129 <robertwang3129@gmail.com>
2023-10-12 20:19:58 +0530

    Whats app url done

--- fronted/src/ReferalComp/referal.jsx
+++ fronted/src/ReferalComp/referal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Referal1 from "./Referal1";
 import ReferalReward from "./Referal&Reward";
 import {
@@ -20,8 +20,37 @@ import Accordionist from "./Accordionist";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 
 function Referal() {
+  const[fname,setFname]=useState("")
+  const[email,setEmail]=useState("")
+  const[phoneNumber,setPhnumber]=useState("")
+  const[rcode,setRcode]=useState("")
+    const [refform,setRefform]=useState({
+        F_name:fname,
+        Email:email,
+        Ph_number:phoneNumber,
+        R_Code:rcode
+    })
+
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
+
+
+  const PhoneNumber="+917379249116"
+  console.log(fname)
+
+  const handleSubmit=async()=>{
+    //  try {
+        
+    //  } catch (error) {
+        
+    //  }
+    const message = "https://masaischool.com";
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, "_blank");
+}
+
+
+
   return (
     <>
       <div
@@ -78,19 +107,34 @@ function Referal() {
                 </DrawerHeader>
 
                 <DrawerBody>
-                  <FormControl isRequired>
-                    <FormLabel>Full Name</FormLabel>
-                    <Input placeholder="Enter full name" />
-                    <FormLabel>Email address</FormLabel>
-                    <Input placeholder="Enter email address" />
-                    <FormLabel>Phone Number</FormLabel>
-                    <Input placeholder="Enter your whatsapp number" />
-                    <FormLabel>Referral Code (Optional)</FormLabel>
-                    <Input placeholder="Enter referral Code" />
-                    <Button mt={4} colorScheme="teal" w={"full"} type="submit">
-                      Submit
-                    </Button>
-                  </FormControl>
+                <FormControl isRequired>
+  <FormLabel>Full Name</FormLabel>
+  <Input placeholder='Enter full name' name='F_name'
+   onChange={(e)=>setFname(e.target.value)}
+    />
+  <FormLabel>Email address</FormLabel>
+  <Input placeholder='Enter email address' name='Email'
+   onChange={(e)=>setEmail(e.target.value)}
+    />
+  <FormLabel>Phone Number</FormLabel>
+  <Input placeholder='Enter your whatsapp number' name='Ph_number' 
+  onChange={(e)=>setPhnumber(e.target.value)}
+
+  />
+  <FormLabel>Referral Code (Optional)</FormLabel>
+  <Input placeholder='Enter referral Code' name='R_Code'
+   onChange={(e)=>setRcode(e.target.value)}
+    />
+  <Button
+            mt={4}
+            colorScheme='teal'
+            w={'full'}
+            type='submit'
+            onClick={handleSubmit}
+          >
+            Submit
+          </Button>
+</FormControl>
                 </DrawerBody>
               </DrawerContent>
             </Drawer>
