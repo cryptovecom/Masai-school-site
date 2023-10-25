d11f45cc194e54f95f834bc8b0d7fd0858306131
dreamAI0129 <robertwang3129@gmail.com>
2023-10-25 23:41:43 +0530

    event css done

--- fronted/src/Events/Events.jsx
+++ fronted/src/Events/Events.jsx
@@ -69,15 +69,11 @@ const Events = () => {
     <div>
       <div className="cont">
         <div>
-          <h1 className="font-bold text-5xl pt-20 head">
+          <h1 className="font-bold flex justify-center text-5xl pt-20 head">
             Webinars & Masterclasses
           </h1>
           <p className="text-center text-wrap p_here text-2xl opacity-80 text-secondary">
-            Learn from industry experts and test your skills against peers
-            <br />
-            during our comprehensive events, masterclasses & contests.
-            <br />
-            Experience impactful learning with Masai by your side.
+            Learn from industry experts and test your skills against peers during our comprehensive events, masterclasses & contests. Experience impactful learning with Masai by your side.
           </p>
         </div>
 
@@ -106,8 +102,8 @@ const Events = () => {
             </button>
           </div>
 
-          <div class="max-w-md mx-auto">
-            <div class="pr-28 relative flex items-center w-full h-14 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+          <div class="max-w-md search_div mx-auto">
+            <div class="pr-28 search_ipt relative flex items-center w-full h-14 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
               <div class="grid place-items-center h-full w-14 text-gray-300">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -140,8 +136,8 @@ const Events = () => {
       {eventData?.map((elem) => (
         <div className="parent">
           <div className="child-1 border">
-            <h3 className=".text-secondary">{elem.date}</h3>
-            <p className="font-bold flex justify-center month_p">{elem.month}</p>
+           <div><h3 className=".text-secondary event_date">{elem.date}</h3></div>
+           <div> <p className="font-bold flex justify-center month_p">{elem.month}</p></div>
             
           </div>
 
@@ -149,7 +145,7 @@ const Events = () => {
             <div className="img_div">
               <img
                 src={elem.img}
-                width={500}
+                // width={500}
                 className="myImage"
                 // className="w-50"
                 alt=""
@@ -157,18 +153,22 @@ const Events = () => {
             </div>
 
             <div className="data_div">
-              <p className="fs-5 font-bold text-xl">{elem.title}</p>
-              <p className="font-bold text-3xl posi_tion ">{elem.position}</p>
+              <div>
+              <div><p className="fs-5 font-bold text-xl">{elem.title}</p></div>
+              <div><p className="font-bold text-3xl posi_tion ">{elem.position}</p></div>
+              </div>
+              
               <div className="myData_div">
                 <div className="mt-4">
-                  <p className="hour_p font-medium">{elem.duration}</p>
+                 <div><p className="hour_p font-medium">{elem.duration}</p></div>
 
                   <div className="time_data">
                     <div className="time-1">
-                      <p className="font-medium">{elem.start_time}</p>
-                      <p className="font-medium pm-1">{elem.type1}</p>
+                      <div><p className="font-medium">{elem.start_time}</p></div>
+                      <div><p className="font-medium pm-1">{elem.type1}</p></div>
                     </div>
-
+                    
+                    <div className="circle_svg">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="14"
@@ -179,15 +179,17 @@ const Events = () => {
                     >
                       <circle cx="8" cy="8" r="8" />
                     </svg>
+                    </div>
+
                     <hr class="border-2 opacity-200 my_hr"></hr>
 
                     <div className="time-2">
-                      <p className="font-medium">{elem.end_time}</p>
-                      <p className="font-medium pm-2">{elem.type2}</p>
+                      <div><p className="font-medium">{elem.end_time}</p></div>
+                     <div><p className="font-medium pm-2">{elem.type2}</p></div> 
                     </div>
                   </div>
 
-                  <p className="duration_p font-medium">Duration</p>
+                 <div><p className="duration_p font-medium">Duration</p></div> 
                 </div>
                 {/* <div className="rec_div">
                 <button type="button" class="btn fw-medium rec_btn">RECORDING</button>
@@ -195,6 +197,7 @@ const Events = () => {
               </div>
               <hr className="hr_here" />
               <div className="btm_div">
+                <div className="fire_svg mt-1 ml-3">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -205,9 +208,10 @@ const Events = () => {
                 >
                   <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16Zm0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15Z" />
                 </svg>
+                </div>
 
-                <p className=" std_num font-medium">{elem.people}</p>
-                <p className="my_txt font-medium">people have registered</p>
+                <div><p className="std_num font-medium">{elem.people}</p></div>
+                 <div><p className="my_txt font-medium">people have registered</p></div>
               </div>
             </div>
 
@@ -216,13 +220,13 @@ const Events = () => {
                 elem.btn == "RECORDING" ? <button
                 type="button"
                 onClick={() => redirect_details(elem._id)}
-                class="text-white bg-red-700 hover:bg-red-500  font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-500 focus:outline-none "
+                class="text-white bg-red-700 hover:bg-red-500 reg_btn font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-500 focus:outline-none "
               >
                 {elem.btn}
               </button> : <button
                 type="button"
                 onClick={() => redirect_register(elem._id)}
-                class="text-white bg-red-700 hover:bg-red-500  font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-500 focus:outline-none "
+                class="text-white bg-red-700 hover:bg-red-500 reg_btn font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-500 focus:outline-none "
               >
                 {elem.btn}
               </button>
