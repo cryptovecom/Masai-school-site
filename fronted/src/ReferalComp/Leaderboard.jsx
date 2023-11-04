e8e3c0827942b812f5e40b617c155ebd09dd8568
dreamAI0129 <robertwang3129@gmail.com>
2023-11-04 23:32:16 +0530

    Shipping cart

--- fronted/src/ReferalComp/Leaderboard.jsx
+++ fronted/src/ReferalComp/Leaderboard.jsx
@@ -1,46 +1,14 @@
 import React, { useState } from "react";
 import Invite from "./Invite";
-import Profile_Rewards from "./Profile_Rewards";
-import RewardsLeaderBoards from "./RewardsLeaderBoards";
+
+import AddressPage from "./Addresspage";
 
 function Leaderboard() {
   const [page,setPage]=useState(1)
   return (
     <div className="">
-      {/* <div className="w-100" style={{width:"100%"}} >
-       
-          <div className="flex"  style={{ border: "1px solid red", width: "100%" }}>
-            <div>
-              <button className="border-solid border-black hover:bg-red-50 hover:text-red-400 p-2 font-bold ml-2 hover:text-red-600 focus:text-[#777bf2]"
-             onClick={()=>setPage(1)}
-              >
-                Invite
-              </button>
-            </div>
-            <div>
-              <button className=" hover:bg-red-50 hover:text-red-400 p-2 font-bold ml-2 hover:text-red-600 focus:text-[#777bf2]"
-              onClick={()=>setPage(2)}
-              >
-                Rewards
-              </button>
-            </div>
-            <div>
-              <button className=" hover:bg-red-50 hover:text-red-400 p-2 font-bold ml-2 hover:text-red-600 focus:text-[#777bf2]"
-                   onClick={()=>setPage(3)}
-              >
-                referrals
-              </button>
-            </div>
-          </div>
-        
-        <div>
-       
-       {page===2?<RewardsLeaderBoards/>:<></>}
-       {page===3?<Profile_Rewards/>:<></>}
-      
-        </div>
-      </div> */}
       <Invite/>
+      <AddressPage/>
     </div>
   );
 }
