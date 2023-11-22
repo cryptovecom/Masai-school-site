5c421e3168430c5e64bd6c0323be7fb6d0a3d99e
dreamAI0129 <robertwang3129@gmail.com>
2023-11-22 20:43:06 +0530

    Cart

--- fronted/src/ReferalComp/Addresspage.jsx
+++ fronted/src/ReferalComp/Addresspage.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import { FaAddressCard } from 'react-icons/fa'
 import AddressPageCard from './AddressPageCard'
+import { useSelector } from 'react-redux'
 
 function AddressPage() {
-  const [bool,setBool]=useState(false)
+
   const [arr,setArr]=useState([])
+  const cart=useSelector(state=>state.Reward.cartStatus)
+  console.log(cart)
   return (
     <div className='rounded-md'  style={{width:"70%",margin:"auto",marginTop:"20px", boxShadow:"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
       backgroundImage: 'url("https://img.freepik.com/free-photo/delivery-trucks-road-out-smartphone-carry-goods-customer-home-with-location-pointer-bubble-chat-message-ecommerce-concept-blue-background-3d-illustration_56104-1811.jpg?size=626&ext=jpg&ga=GA1.1.144022409.1691515271&semt=ais")', 
@@ -13,9 +16,10 @@ function AddressPage() {
   backgroundPosition: 'center center', 
     }}>
     <h3 className='text-center md:text-[24px] font-[500] text-[12px]'>Shipping Status</h3>
-    {bool ? (
-      <div className='flex justify-center'>
-        <img src='https://mir-s3-cdn-cf.behance.net/projects/404/c1a552112421559.Y3JvcCwxMzczLDEwNzQsMzMsMA.jpg' alt='' />
+    {cart ? (
+      <div className='flex justify-center' style={{width:"100%"}}>
+        <img   src='https://cdn4.avada.io/media/shopify/ONkV09b.png' alt='' />
+      
       </div>
     ) : (
       
